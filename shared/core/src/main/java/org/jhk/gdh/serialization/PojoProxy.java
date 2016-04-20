28c7c70f929d54f9edeece8d705c97b5feb4d729
JKArena <JihoonKim1004@gmail.com>
2016-04-20 10:00:55 -0700

    Just so won't lose
    Will need to touch up later

---
+++ shared/core/src/main/java/org/jhk/gdh/serialization/PojoProxy.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jhk.gdh.serialization;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @author Ji Kim
+ */
+public final class PojoProxy<T> implements InvocationHandler {
+    
+    private T _obj;
+    
+    public PojoProxy(T obj) {
+        super();
+        
+        _obj = obj;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return method.invoke(_obj, args);
+    }
+    
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public static <T> T getInstance(Class<T> clazz, Object object) {
+        return (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{clazz}, new PojoProxy(object));
+    }
+    
+}
