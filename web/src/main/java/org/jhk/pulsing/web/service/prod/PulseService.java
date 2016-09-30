d307608f10db73a534786c5af57cefa72dcaab62
JKArena <JihoonKim1004@gmail.com>
2016-09-29 18:52:31 -0700

    removal of dev mode

--- web/src/main/java/org/jhk/pulsing/web/service/prod/PulseService.java
+++ web/src/main/java/org/jhk/pulsing/web/service/prod/PulseService.java
@@ -22,7 +22,6 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Map;
 import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
