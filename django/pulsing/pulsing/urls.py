de0e4f4771c0bd7865f6878ec47c95f0622adfa0
JHKTruth <Jihoon.Kim.Truth@gmail.com>
2017-03-28 01:34:58 -0700

    PyDev since sublime is a bit lacking

--- django/pulsing/pulsing/urls.py
+++ django/pulsing/pulsing/urls.py
@@ -16,6 +16,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-  url(r'^admin/', include(admin.site.urls)),
-  url(r'^location/', include('location.urls', namespace='pulsing-location', app_name='location'))
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^location/', include('location.urls', namespace='pulsing-location', app_name='location'))
 ]
