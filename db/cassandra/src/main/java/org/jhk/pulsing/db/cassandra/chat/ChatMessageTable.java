f8503d71a5e4d003773f2b14ec549e0be33c22e3
JKArena <JihoonKim1004@gmail.com>
2017-04-04 21:43:53 -0700

    initial mod to cassandra to allow counting of message views

--- db/cassandra/src/main/java/org/jhk/pulsing/db/cassandra/chat/ChatMessageTable.java
+++ db/cassandra/src/main/java/org/jhk/pulsing/db/cassandra/chat/ChatMessageTable.java
@@ -40,9 +40,13 @@ public final class ChatMessageTable implements ICassandraTable {
     private static final Logger _LOGGER = LoggerFactory.getLogger(ChatMessageTable.class);
     
     private static final String _CHAT_MESSAGE_TABLE = "CHAT_MESSAGE_TABLE";
+    private static final String _CHAT_MESSAGE_VIEW_COUNT_TABLE = "CHAT_MESSAGE_VIEW_COUNT_TABLE";
     
     private final PreparedStatement _CHAT_MESSAGE_QUERY;
     private final PreparedStatement _CHAT_MESSAGE_INSERT;
+    
+    private final PreparedStatement _CHAT_MESSAGE_VIEW_COUNT_QUERY;
+    private final PreparedStatement _CHAT_MESSAGE_VIEW_COUNT_INSERT;
     
     private final Session _session;
     private final String _keySpace;
@@ -57,35 +61,73 @@ public final class ChatMessageTable implements ICassandraTable {
                 .ifNotExists()
                 .addPartitionKey("chat_lobby_id", DataType.timeuuid())
                 .addClusteringColumn("timestamp", DataType.bigint())
+                .addColumn("msg_id", DataType.uuid())
                 .addColumn("user_id", DataType.bigint())
                 .addColumn("message", DataType.text())
                 .withOptions().clusteringOrder("timestamp", SchemaBuilder.Direction.DESC);
         
         _session.execute(cMSchemaStatement);
         
+        SchemaStatement cmvCSchemaStatement = SchemaBuilder.createTable(_CHAT_MESSAGE_VIEW_COUNT_TABLE)
+                .ifNotExists()
+                .addPartitionKey("msg_id", DataType.uuid())
+                .addClusteringColumn("user_id", DataType.bigint())
+                .addColumn("timestamp", DataType.bigint())
+                .withOptions().clusteringOrder("user_id", SchemaBuilder.Direction.DESC);
+        
+        _session.execute(cmvCSchemaStatement);
+        
         _CHAT_MESSAGE_QUERY = _session.prepare("SELECT * FROM " + _CHAT_MESSAGE_TABLE + 
                 " WHERE chat_lobby_id=? AND timestamp < ? LIMIT 40");
         _CHAT_MESSAGE_INSERT = _session.prepare("INSERT INTO " + _CHAT_MESSAGE_TABLE + 
-                " (chat_lobby_id, user_id, timestamp, message) VALUES (?, ?, ?, ?)");
+                " (chat_lobby_id, user_id, msg_id, timestamp, message) VALUES (?, ?, ?, ?, ?)");
+        
+        _CHAT_MESSAGE_VIEW_COUNT_QUERY = _session.prepare("SELECT * FROM " + _CHAT_MESSAGE_VIEW_COUNT_TABLE + 
+                " WHERE msg_id=?");
+        _CHAT_MESSAGE_VIEW_COUNT_INSERT = _session.prepare("INSERT INTO " + _CHAT_MESSAGE_VIEW_COUNT_TABLE + 
+                " (msg_id, timestamp, user_id) VALUES (?, ?, ?)");
     }
     
     public void messageInsert(UUID cLId, long from, long timeStamp, String message) {
         _LOGGER.info("ChatMessageTable.messageInsert : " + cLId + " - " + ";" + message);
         
-        BoundStatement cLMInsert = _CHAT_MESSAGE_INSERT.bind(cLId, from, timeStamp, message);
+        UUID msgId = UUID.randomUUID();
+        BoundStatement cLMInsert = _CHAT_MESSAGE_INSERT.bind(cLId, from, msgId, timeStamp, message);
         _session.executeAsync(cLMInsert);
+        
+        messageViewCountInsert(msgId, timeStamp, from);
     }
     
+    /**
+     * TODO: Use Spark to query CHAT_MESSAGE_VIEW_COUNT_TABLE and map the count in
+     * 
+     * @param cLId
+     * @param timeStamp
+     * @return
+     */
     public ResultSet messageQuery(UUID cLId, Long timeStamp) {
         _LOGGER.info("ChatMessageTable.messageQuery : " + cLId + " - " + timeStamp);
         
         BoundStatement cLMQuery = _CHAT_MESSAGE_QUERY.bind(cLId, timeStamp);
         return _session.execute(cLMQuery);
     }
+    
+    public void messageViewCountInsert(UUID msgId, long timeStamp, long userId) {
+        _LOGGER.info("ChatMessageTable.messageViewCountInsert : " + msgId + " - " + userId + "/" + timeStamp);
+        
+        _session.executeAsync(_CHAT_MESSAGE_VIEW_COUNT_INSERT.bind(msgId, timeStamp, userId));
+    }
+    
+    private ResultSet messageViewCountQuery(UUID msgId) {
+        _LOGGER.info("ChatMessageTable.messageViewCountQuery : " + msgId);
+        
+        return _session.execute(_CHAT_MESSAGE_VIEW_COUNT_QUERY.bind(msgId));
+    }
 
     @Override
     public void destroy() {
         _session.execute(SchemaBuilder.dropTable(_CHAT_MESSAGE_TABLE));
+        _session.execute(SchemaBuilder.dropTable(_CHAT_MESSAGE_VIEW_COUNT_TABLE));
     }
 
 }
