5cfa7c73904be9b7dd592ca0d0d79cd47daa9e4d
JKArena <JihoonKim1004@gmail.com>
2017-04-09 21:00:05 -0700

    pushing to hdfs with pull by hive external later

--- spark/streaming/src/main/scala/org/jhk/pulsing/spark/streaming/LocationStreaming.scala
+++ spark/streaming/src/main/scala/org/jhk/pulsing/spark/streaming/LocationStreaming.scala
@@ -20,6 +20,9 @@ package org.jhk.pulsing.spark.streaming
 
 import org.apache.log4j.{Level, LogManager, PropertyConfigurator}
 import org.apache.spark.SparkConf
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.StreamingContext._
 import org.apache.spark.TaskContext
@@ -29,7 +32,13 @@ import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
-import org.apache.spark.sql.SparkSession
+
+import org.apache.hadoop.mapreduce.Job
+import org.apache.hadoop.io.NullWritable
+import org.apache.avro.specific.SpecificRecord;
+import org.apache.avro.Schema
+import org.apache.avro.mapred.{AvroValue, AvroKey}
+import org.apache.avro.mapreduce.{AvroKeyOutputFormat, AvroKeyValueOutputFormat, AvroJob}
 
 import com.google.maps.GeoApiContext
 import com.google.maps.GeocodingApi
@@ -44,6 +53,7 @@ import org.jhk.pulsing.serialization.avro.serializers.SerializationHelper
  * @author Ji Kim
  */
 object LocationStreaming {
+  val SPARK_LOCATION_CREATE = HadoopConstants.HDFS_URL_PORT + HadoopConstants.SPARK_NEW_DATA_WORKSPACE + "location"
   val CHECKPOINT = HadoopConstants.getWorkingDirectory(HadoopConstants.DIRECTORIES.SPARK_LOCATION_CREATE)
   
   def createStreamingContext() = {
@@ -79,37 +89,42 @@ object LocationStreaming {
       Subscribe[String, String](topics, kafkaParameters)
     )
     
-    stream.foreachRDD{ rdd =>
-      rdd.foreachPartition { records =>
-        
+    stream.foreachRDD{ rdd => {
+      
+      var job = new Job(streamingContext.sparkContext.hadoopConfiguration)
+      AvroJob.setOutputKeySchema(job, Location.getClassSchema)
+          
+      rdd.mapPartitions{ partition =>
+  
         val context = TaskContext.get
         val geoContext = new GeoApiContext().setApiKey(MAP_API_KEY)
-        
-        val sparkSession = SparkSession.builder.master(SPARK_YARN_CLUSTER_MASTER)
-          .appName("Location Create Hive")
-          .enableHiveSupport()
-          .getOrCreate()
-
         val partitionLogger = LogManager.getLogger("Location")
         partitionLogger.info(s"Location Create for partition: ${context.partitionId}")
-        
-        records.foreach(record => {
+  
+        var data = partition.map({ record => 
           //perform avro deserialization + geo api call
-          
           partitionLogger.info("Processing record - " + record)
-          
-          val deserialized = SerializationHelper.deserializeFromJSONStringToAvro(classOf[Location], Location.getClassSchema(), record.value)
+
+          val deserialized = SerializationHelper.deserializeFromJSONStringToAvro(classOf[Location], Location.getClassSchema, record.value)
           val result:Array[GeocodingResult] = GeocodingApi.geocode(geoContext, deserialized.getAddress().toString()).await()
-          
+
           partitionLogger.info("GeocodingResult - " + result.length)
-          
+
           val geoLoc = result(0).geometry.location;
           deserialized.setLat(geoLoc.lat)
           deserialized.setLng(geoLoc.lng)
-          
+
           partitionLogger.info("Deserialized - " + deserialized)
-        })
-      
+          (new AvroKey(deserialized), NullWritable.get())
+        }).toList
+
+        data.iterator
+      }.saveAsNewAPIHadoopFile(SPARK_LOCATION_CREATE, 
+          classOf[AvroKey[Location]], 
+          classOf[NullWritable], 
+          classOf[AvroKeyOutputFormat[Location]], 
+          job.getConfiguration)
+
       }
     }
     
