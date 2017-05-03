3a5b60481c4c181ec93e61754f328431858d8cde
JKArena <Jihoon.Kim.Truth@gmail.com>
2017-05-03 16:02:42 -0700

    initial friend request good w/ django + spring redis cache sync up

--- django/pulsing/friend/views.py
+++ django/pulsing/friend/views.py
@@ -23,31 +23,47 @@ import logging
 import uuid
 
 from django.http import JsonResponse, HttpResponseBadRequest
-from django.core.cache import get_cache, cache
+from django.core.cache import cache
 
 from shared.models import User
+from shared.redis import Redis
 
 logger = logging.getLogger(__name__)
+redis = Redis()
 
-def friendRequest(request, userId, friendId):
+def friendInvitationId(friendId):
+    return '_'.join(['FRIEND_REQUEST_INVITE', friendId, str(uuid.uuid4())])
+
+#{"expiration":1493846911462,"invitationId":"CHAT_LOBBY_INVITE__2_40372843577401","fromUserId":1,"invitationType":"CHAT_LOBBY_INVITE_"}
+
+def friendRequest(request):
     """ 
     technically does not have to perform the round trip as can pass off
     to websocket on the spring side; however to play around with redis initially
     with django am sending the request here with websocket controller only sending
     out the system alert
     """
-    logger.debug('friendRequest %s- %s/%s ', userId, friendId)
+    parameters = [field for field in ['userId', 'friendId'] if not field in request.POST]
     
-    userKey = 'user_' + userId
-    friendKey = 'user_' + friendId
+    if len(parameters) > 0:
+        logger.debug('queryDocument lacking parameters - %s', parameters)
+        return HttpResponseBadRequest()
     
+    userId = request.POST['userId']
+    friendId = request.POST['friendId']
+    
+    logger.debug('friendRequest %s- %s ', userId, friendId)
+    
+    # TODO need to check if they are already friends or not
     user = User.objects.get_user(id=userId)
     friend = User.objects.get(id=friendId)
-    # TODO need to check if they are friends or not
     
-    redis_cache = get_cache('redis')
-    # TODO push the invitation id to the redis cache to sync with Spring
-    invitation_id = uuid.uuid4()
+    invitation_id = friendInvitationId(friendId)
+    redis.storeInvitation(friendId, invitation_id, 'FRIEND_REQUEST_INVITE')
+    
+    invitations = redis.client.smembers('INVITATIONS_'+friendId) 
+    
+    logger.debug('Previous invitations %s', invitations)
     
     return JsonResponse({
         'code': 'SUCCESS',
