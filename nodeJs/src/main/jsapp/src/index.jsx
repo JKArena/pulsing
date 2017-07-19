8d2284f58d386378964913bd0ec3bdf4d3fa4fbe
JKArena <Jihoon.Kim.Truth@gmail.com>
2017-07-18 23:09:47 -0400

    cleanup

--- nodeJs/src/main/jsapp/src/index.jsx
+++ nodeJs/src/main/jsapp/src/index.jsx
@@ -27,29 +27,13 @@ import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 
-import App from './components/App';
+import AppContainer from './components/AppContainer';
 import indexStore from './indexStore';
-import TrendingPulseSubscriptionsComponent from './components/pulsing/trending/TrendingPulseSubscriptionsComponent';
-import MapComponent from './components/map/MapComponent';
-import CreatePulseComponent from './components/pulsing/create/CreatePulseComponent';
-import CreateLocationComponent from './components/location/create/CreateLocationComponent';
-import AlertListingComponent from './components/alert/AlertListingComponent';
-import SignupComponent from './components/signup/SignupComponent';
-import LoginComponent from './components/login/LoginComponent';
 
 // Render the app component into the dom
 render((
   <Provider store={indexStore}>
     <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={TrendingPulseSubscriptionsComponent} />
-        <Route path="map/:store" component={MapComponent} />
-        <Route path="createPulse" component={CreatePulseComponent} />
-        <Route path="createLocation" component={CreateLocationComponent} />
-        <Route path="alertListing" component={AlertListingComponent} />
-        <Route path="signup" component={SignupComponent} />
-        <Route path="login" component={LoginComponent} />
-      </Route>
     </Router>
   </Provider>
 ), document.getElementById('app'));
