cb02593722c03351adab75afb53ff8868d4aafc3
JKArena <JihoonKim1004@gmail.com>
2018-03-28 22:53:26 -0400

    user cache sandbox

---
+++ sandbox/src/main/java/org/jhk/pulsing/sandbox/timeline/util/Util.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jhk.pulsing.sandbox.timeline.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Ji Kim
+ */
+public final class Util {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(Util.class);
+    
+    public static void readStream(InputStream stream, Consumer<String> lineConsumer) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))) {
+            String line = null;
+            
+            while((line = reader.readLine()) != null) {
+                lineConsumer.accept(line);
+            }
+        } catch(IOException io) {
+            LOGGER.error(io.getMessage(), io);
+        }
+    }
+    
+    private Util() {}
+}
