a286b6be08fdb8a18ef75be26e51a80eec3dd91d
JKArena <JihoonKim1004@gmail.com>
2018-04-27 20:46:50 -0400

    trainer

--- tensorFlow/sandbox/shared/trainers.py
+++ tensorFlow/sandbox/shared/trainers.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
 
 def linear_regression(model, label):
   lin_reg = LinearRegression()
@@ -23,6 +24,22 @@ def random_forest_regressor(model, label):
   """
   forest_reg = RandomForestRegressor()
   forest_reg.fit(model, labels)
+
+def grid_search_cv(forest_reg, param_grid, cv, scoring):
+  """One way to fine tune a model would be to fiddle with the hyperparameters manually, until you find 
+  a great combination of hyperparameter values. This would be very tedious work, so instead use GridSearchCV for 
+  the search. Need to tell it which hyperparameters you want it to experiement with, what values to try out, and 
+  it will evaluate all the possible combinations of hyperparameter values, using cross-validation.
+  param_grid = [
+    {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+    {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]}
+  ]
+  Note grid search approach is fine when exploring relatively few combinations, but when hyperparameter search space 
+  is large, it is often preferable to use RandomizedSearchCV instead. Instead of trying out all possible combinations,
+  it evaluates a given number of random combinations by selecting a random value for each hyperparameter at every 
+  iteration
+  """
+  grid_search = GridSearchCV(forest_reg, param_grid, cv=cv, scoring=scoring)
 
 def cross_score(tree_reg, model, label, scoring, cv):
   """Performs K-fold cross validation: randomly splits the training set into cv (i.e. 10)
