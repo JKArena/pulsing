e9d181cde7457b3b12b61ef781403cd1d3a55e30
JKArena <Jihoon.Kim.Truth@gmail.com>
2018-07-04 17:42:48 -0400

    some skeleton/base classes which will be used.

---
+++ zookeeper/src/main/java/org/jhk/pulsing/zookeeper/LeaderBase.java
@@ -0,0 +1,113 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jhk.pulsing.zookeeper;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.AsyncCallback.DataCallback;
+import org.apache.zookeeper.AsyncCallback.StringCallback;
+import org.apache.zookeeper.KeeperException.Code;
+import org.apache.zookeeper.data.Stat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Ji Kim
+ */
+public abstract class LeaderBase extends AbstractBase {
+    
+    private static final Logger _LOGGER = LoggerFactory.getLogger(LeaderBase.class);
+    
+    private final String path;
+    private final byte[] data;
+    
+    private boolean leader;
+    
+    private final StringCallback LEADER_RUN_CALLBACK = new StringCallback() {
+        @Override
+        public void processResult(int responseCode, String path, Object context, String name) {
+            switch (Code.get(responseCode)) {
+            case CONNECTIONLOSS:
+                /*
+                 * when client becomes disconnected from a ZooKepper server. Network error, such as network partition,
+                 * or the failure of a ZooKeeper server. Unknown to the client whether the request was lost before 
+                 * the ZooKeeper servers processed it or if they processed it but the client did not receive the response.
+                 * 
+                 * ZooKeeper client will reestablish the connection for future request, but must figure out whether a pending 
+                 * request has been processed or whether it should reissue the request.
+                 */
+                checkIsLeader();
+                break;
+            case OK:
+                _LOGGER.error("Is leader for path {}.", path);
+                leader = true;
+                break;
+            default:
+                _LOGGER.error("Is NOT leader for path {}.", path);
+                leader = false;
+            }
+        }
+    };
+    
+    private DataCallback LEADER_CHECK_CALLBACK = new DataCallback() {
+        @Override
+        public void processResult(int responseCode, String path, Object context, byte[] data, Stat stat) {
+            switch (Code.get(responseCode)) {
+            case CONNECTIONLOSS:
+                checkIsLeader();
+                break;
+            case NONODE:
+                runForLeader();
+                break;
+            default:
+                _LOGGER.error("Error code {} in leader check with path {}.", Code.get(responseCode), path);
+                break;
+            }
+        }
+    };
+    
+    public LeaderBase(String path, byte[] data) {
+        super();
+        
+        this.path = path;
+        this.data = data;
+    }
+    
+    public LeaderBase(String path, byte[] data, String hostPort, int sessionTimeout) {
+        super(hostPort, sessionTimeout);
+        
+        this.path = path;
+        this.data = data;
+    }
+    
+    public boolean isLeader() {
+        return leader;
+    }
+
+    public void runForLeader() {
+        zookeeper.create(path,  data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL, LEADER_RUN_CALLBACK, getStat());
+    }
+    
+    private void checkIsLeader() {
+        zookeeper.getData(path, false, LEADER_CHECK_CALLBACK, getStat());
+    }
+    
+    protected abstract Stat getStat();
+    
+}
---
+++ zookeeper/src/main/java/org/jhk/pulsing/zookeeper/StateBase.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jhk.pulsing.zookeeper;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.Arrays;
+
+import org.apache.zookeeper.AsyncCallback.StatCallback;
+import org.apache.zookeeper.AsyncCallback.StringCallback;
+import org.apache.zookeeper.KeeperException.Code;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Ji Kim
+ */
+public abstract class StateBase extends AbstractBase {
+    
+    private static final Logger _LOGGER = LoggerFactory.getLogger(StateBase.class);
+    
+    private final StringCallback INITIAL_SETUP_CALLBACK = new StringCallback() {
+        @Override
+        public void processResult(int responseCode, String path, Object context, String name) {
+            switch (Code.get(responseCode)) {
+            case CONNECTIONLOSS:
+                initialSetup();
+                break;
+            case OK:
+                _LOGGER.error("Setup properly for path {} with state {}.", path, context);
+                break;
+            case NODEEXISTS:
+                break;
+            default:
+                _LOGGER.error("Error code {} in creating bootstrap with path {}.", Code.get(responseCode), path);
+            }
+        }
+    };
+    
+    private final StatCallback STATE_UPDATE_CALLBACK = new StatCallback() {
+        @Override
+        public void processResult(int responseCode, String path, Object context, Stat stat) {
+            switch(Code.get(responseCode)) {
+            case CONNECTIONLOSS:
+                updateState((byte[]) context);
+                break;
+            }
+        }
+    };
+    
+    private final String path;
+    private byte[] data;
+    
+    public StateBase(String path, byte[] data) {
+        super();
+        
+        this.path = path;
+        this.data = data;
+    }
+    
+    public StateBase(String path, byte[] data, String hostPort, int sessionTimeout) {
+        super(hostPort, sessionTimeout);
+        
+        this.path = path;
+        this.data = data;
+    }
+    
+    public void initialSetup() {
+        zookeeper.create(path,  data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL, INITIAL_SETUP_CALLBACK, data);
+    }
+    
+    private synchronized void updateState(byte[] data) {
+        /*
+         * Since using asynchronous request with retry on connection loss, things may get out of order. So before making a request 
+         * update, ensure that one is requeuing the current status; otherwise abort.
+         */
+        if (Arrays.equals(this.data, data)) {
+            //unconditional update with version of -1.
+            zookeeper.setData(path,  data, -1, STATE_UPDATE_CALLBACK, data);            
+        }
+    }
+    
+    public void setState(byte[] data) {
+        this.data = data;
+    }
+    
+}
