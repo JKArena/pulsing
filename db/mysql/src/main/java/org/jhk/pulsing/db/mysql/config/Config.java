41e6a2be981ab9da528f404865b08e3e4881d93f
JKArena <JihoonKim1004@gmail.com>
2019-05-11 23:51:20 -0400

    move over orm pom and temporary inclusion of client (push out later)

---
+++ db/mysql/src/main/java/org/jhk/pulsing/db/mysql/config/Config.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jhk.pulsing.db.mysql.config;
+
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.jhk.pulsing.db.mysql.user.MySqlUserDao;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author Ji Kim
+ */
+@EnableTransactionManagement
+@Configuration
+@PropertySource({"classpath:application.properties"})
+public class Config {
+
+    @Inject
+    private Environment env;
+    
+    @Bean(name="mySqlUserDao")
+    public MySqlUserDao getMySqlUserDao() {
+        return new MySqlUserDao();
+    }
+    
+    @Bean
+    public PlatformTransactionManager transactionManager() throws SQLException {
+        HibernateTransactionManager tManager = new HibernateTransactionManager();
+        tManager.setSessionFactory(sessionFactory().getObject()); 
+        return tManager;
+    }
+    
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sFactory = new LocalSessionFactoryBean();
+        sFactory.setDataSource(dataSource());
+        sFactory.setPackagesToScan(new String[] {"org.jhk.pulsing.db.mysql.model"});
+        sFactory.setHibernateProperties(hibernateProperties());
+        return sFactory;
+    }
+    
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getRequiredProperty("datasource.driver_class"));
+        dataSource.setUrl(env.getRequiredProperty("datasource.url"));
+        dataSource.setUsername(env.getRequiredProperty("datasource.user"));
+        dataSource.setPassword(env.getRequiredProperty("datasource.password"));
+        return dataSource;
+    }
+    
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.hbm2ddl.auto", env.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
+        properties.put("hibernate.format_sql", env.getRequiredProperty("hibernate.format_sql"));
+        properties.put("hibernate.cache.provider_class", env.getRequiredProperty("hibernate.cache.provider_class"));
+        properties.put("hibernate.archive.autodetection", env.getRequiredProperty("hibernate.archive.autodetection"));
+        properties.put("hibernate.c3p0.min_size", env.getRequiredProperty("hibernate.c3p0.min_size"));
+        properties.put("hibernate.c3p0.max_size", env.getRequiredProperty("hibernate.c3p0.max_size"));
+        properties.put("hibernate.c3p0.timeout", env.getRequiredProperty("hibernate.c3p0.timeout"));
+        properties.put("hibernate.c3p0.max_statements", env.getRequiredProperty("hibernate.c3p0.max_statements"));
+        properties.put("hibernate.c3p0.idle_test_period", env.getRequiredProperty("hibernate.c3p0.idle_test_period"));
+        properties.put("hibernate.id.new_generator_mappings", env.getRequiredProperty("hibernate.id.new_generator_mappings"));
+        return properties;
+    }
+    
+}
--- spring/src/main/java/org/jhk/pulsing/web/service/ProdServiceConfig.java
+++ spring/src/main/java/org/jhk/pulsing/web/service/ProdServiceConfig.java
@@ -18,18 +18,11 @@
  */
 package org.jhk.pulsing.web.service;
 
-import java.sql.SQLException;
-import java.util.Properties;
-
-import javax.inject.Inject;
-import javax.sql.DataSource;
-
 import org.jhk.pulsing.web.dao.prod.db.redis.RedisPulseDao;
 import org.jhk.pulsing.web.dao.prod.db.redis.RedisUserDao;
 import org.jhk.pulsing.web.service.prod.InvitationService;
-import org.jhk.pulsing.db.mysql.user.MySqlUserDao;
-import org.jhk.pulsing.chat.ChatService;
-import org.jhk.pulsing.chat.IChatService;
+import org.jhk.pulsing.client.chat.internal.ChatService;
+import org.jhk.pulsing.client.chat.IChatService;
 import org.jhk.pulsing.client.pulse.IPulseService;
 import org.jhk.pulsing.client.pulse.internal.PulseService;
 import org.jhk.pulsing.client.user.IUserService;
@@ -38,11 +31,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -58,9 +46,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @PropertySource({"classpath:application.properties"})
 public class ProdServiceConfig implements IServiceConfig {
-    
-    @Inject
-    private Environment env;
     
     @Bean
     @Override
@@ -94,53 +79,6 @@ public class ProdServiceConfig implements IServiceConfig {
     @Bean(name="redisUserDao")
     public RedisUserDao getRedisUserDao() {
         return new RedisUserDao();
-    }
-    
-    @Bean(name="mySqlUserDao")
-    public MySqlUserDao getMySqlUserDao() {
-        return new MySqlUserDao();
-    }
-    
-    @Bean
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sFactory = new LocalSessionFactoryBean();
-        sFactory.setDataSource(dataSource());
-        sFactory.setPackagesToScan(new String[] {"org.jhk.pulsing.db.mysql.model"});
-        sFactory.setHibernateProperties(hibernateProperties());
-        return sFactory;
-    }
-    
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getRequiredProperty("datasource.driver_class"));
-        dataSource.setUrl(env.getRequiredProperty("datasource.url"));
-        dataSource.setUsername(env.getRequiredProperty("datasource.user"));
-        dataSource.setPassword(env.getRequiredProperty("datasource.password"));
-        return dataSource;
-    }
-    
-    @Bean
-    public PlatformTransactionManager transactionManager() throws SQLException {
-        HibernateTransactionManager tManager = new HibernateTransactionManager();
-        tManager.setSessionFactory(sessionFactory().getObject()); 
-        return tManager;
-    }
-    
-    private Properties hibernateProperties() {
-        Properties properties = new Properties();
-        properties.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
-        properties.put("hibernate.hbm2ddl.auto", env.getRequiredProperty("hibernate.hbm2ddl.auto"));
-        properties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
-        properties.put("hibernate.format_sql", env.getRequiredProperty("hibernate.format_sql"));
-        properties.put("hibernate.cache.provider_class", env.getRequiredProperty("hibernate.cache.provider_class"));
-        properties.put("hibernate.archive.autodetection", env.getRequiredProperty("hibernate.archive.autodetection"));
-        properties.put("hibernate.c3p0.min_size", env.getRequiredProperty("hibernate.c3p0.min_size"));
-        properties.put("hibernate.c3p0.max_size", env.getRequiredProperty("hibernate.c3p0.max_size"));
-        properties.put("hibernate.c3p0.timeout", env.getRequiredProperty("hibernate.c3p0.timeout"));
-        properties.put("hibernate.c3p0.max_statements", env.getRequiredProperty("hibernate.c3p0.max_statements"));
-        properties.put("hibernate.c3p0.idle_test_period", env.getRequiredProperty("hibernate.c3p0.idle_test_period"));
-        properties.put("hibernate.id.new_generator_mappings", env.getRequiredProperty("hibernate.id.new_generator_mappings"));
-        return properties;
     }
     
 }
